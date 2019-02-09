b8138990141ec5a605becef357832ee1f8f7f7fb
Nathan <nathan@Nathans-Macbook-Pro.local>
2019-02-09 16:51:59 -0500

    Remove db class

--- invertedFile.py
+++ invertedFile.py
@@ -3,34 +3,6 @@ import InvertedIndex
 import InvertedIndexQuery
 nltk.download()
 
-class Database:
-    """
-    In memory database representing the already indexed documents.
-    """
-    def __init__(self):
-        self.db = dict()
-
-    def __repr__(self):
-        """
-        String representation of the Database object
-        """
-        return str(self.__dict__)
-    
-    def get(self, id):
-        return self.db.get(id, None)
-    
-    def add(self, document):
-        """
-        Adds a document to the DB.
-        """
-        return self.db.update({document['id']: document})
-
-    def remove(self, document):
-        """
-        Removes document from DB.
-        """
-        return self.db.pop(document['id'], None)
-
 class InvertedIndex:
     """
     Inverted Index class.
