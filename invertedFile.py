64fdcd81ab7435fcb98114bb1be589524b258ee3
nathanml <34776276+nathanml@users.noreply.github.com>
2019-02-09 16:45:12 -0500

    Add files via upload

---
+++ invertedFile.py
@@ -0,0 +1,97 @@
+import nltk
+import InvertedIndex
+import InvertedIndexQuery
+nltk.download()
+
+class Database:
+    """
+    In memory database representing the already indexed documents.
+    """
+    def __init__(self):
+        self.db = dict()
+
+    def __repr__(self):
+        """
+        String representation of the Database object
+        """
+        return str(self.__dict__)
+    
+    def get(self, id):
+        return self.db.get(id, None)
+    
+    def add(self, document):
+        """
+        Adds a document to the DB.
+        """
+        return self.db.update({document['id']: document})
+
+    def remove(self, document):
+        """
+        Removes document from DB.
+        """
+        return self.db.pop(document['id'], None)
+
+class InvertedIndex:
+    """
+    Inverted Index class.
+    """
+    def __init__(self, db):
+        self.index = dict()
+        self.db = db
+
+    def __repr__(self):
+        """
+        String representation of the Database object
+        """
+        return str(self.index)
+        
+    def index_document(self, document):
+        """
+        Process a given document, save it to the DB and update the index.
+        """
+        
+        # Remove punctuation from the text.
+        clean_text = re.sub(r'[^\w\s]','', document['text'])
+        terms = clean_text.split(' ')
+        appearances_dict = dict()
+        # Dictionary with each term and the frequency it appears in the text.
+        for term in terms:
+            term_frequency = appearances_dict[term].frequency if term in appearances_dict else 0
+            appearances_dict[term] = Appearance(document['id'], term_frequency + 1)
+            
+        # Update the inverted index
+        update_dict = { key: [appearance]
+                       if key not in self.index
+                       else self.index[key] + [appearance]
+                       for (key, appearance) in appearances_dict.items() }
+        self.index.update(update_dict)
+        # Add the document into the database
+        self.db.add(document)
+        return document
+    
+    def lookup_query(self, query):
+        """
+        Returns the dictionary of terms with their correspondent Appearances. 
+        This is a very naive search since it will just split the terms and show
+        the documents where they appear.
+        """
+        return { term: self.index[term] for term in query.split(' ') if term in self.index }
+
+
+invertedFile = InvertedIndex.Index()
+
+filename = "tweets.txt"
+
+stopWords = ["the", "a", "an", "by", "for", "on", "if", "at", "in", ]
+
+def removeStopWords(d):
+
+
+file_to_index = open(filename).read()
+document_key = filename
+
+invertedFile.index(file_to_index, document_key)
+
+def defineWord(w):
+	for word in invertedFile:
+		if word == w
